522106facbc5995c3a993d234e89286c6ab0be73
Alexander Karpov <x1te.rus@gmail.com>
2016-05-31 15:28:15 +0300

    Initial Commit

---
+++ StepicNotificationTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StepicNotificationTest
+//
+//  Created by Alexander Karpov on 31.05.16.
+//  Copyright © 2016 Stepic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StepicNotificationTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StepicNotificationTest
+//
+//  Created by Alexander Karpov on 31.05.16.
+//  Copyright © 2016 Stepic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
